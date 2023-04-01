23ccd4376d83ec9ad13dd72185c382e9d193fcf5
Habibou Moussa <habibou997@gmail.com>
2023-04-01 16:50:42 +0000

    created user model

--- auth/users/models.py
+++ auth/users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractBaseUser):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    username = None #Overide the default username
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
