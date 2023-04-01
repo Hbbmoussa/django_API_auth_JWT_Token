a7fa06545df73b599fefd03fedaa1b1ff0d8f1cd
Habibou Moussa <habibou997@gmail.com>
2023-04-01 22:29:33 +0000

    login endpoint added

--- auth/users/views.py
+++ auth/users/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
 from .serializers import UserSerializer
+from .models import User
 # Create your views here.
 
 class RegisterView(APIView):
@@ -9,3 +11,22 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+class LoginView(APIView):
+    def post(self,request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('user not found')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('incorrect password')
+
+        return Response({
+            'message' : 'success'
+        })
+
+
